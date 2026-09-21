5dc366863a2a26db6ee4f3173e1473a94562d26a
Hruthikabhavana <hruthikabhavana@gmail.com>
2026-09-21 11:08:28 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/welcome", (req, res)=>{
+    res.send("welcome back")
+})
+
+app.listen(3000, ()=>{
+    console.log("listening to the port")
+})
